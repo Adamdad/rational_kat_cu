f116f88efacc110c726e96f524c4435b07f081b0
Adamdad <787707188@qq.com>
2024-07-27 17:30:52 +0800

    test_backward(x, numerator_weights, denominator_weights)

--- test.py
+++ test.py
@@ -140,6 +140,6 @@ if __name__=="__main__":
     x = torch.randn(100, 100, dtype=torch.float32, device='cuda', requires_grad=True)
     
     
-    test_forward(x, numerator_weights, denominator_weights)
+    # test_forward(x, numerator_weights, denominator_weights)
 
     test_backward(x, numerator_weights, denominator_weights)
