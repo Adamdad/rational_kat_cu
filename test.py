01d47b7fb8ed60c3bcefbd57dd0d47b1103b74f2
Adamdad <787707188@qq.com>
2024-07-29 14:34:02 +0800

    0.2, 0.3, 0.4, 0.5], dtype=torch.float32,

--- test.py
+++ test.py
@@ -233,7 +233,7 @@ def benchmark_fwd_time(x, numerator_weights, denominator_weights):
     return result
 if __name__=="__main__":
     # Define tensors for the numerator and denominator coefficients
-    numerator_weights = nn.Parameter(torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5, 0.1], dtype=torch.float32, device='cuda')/10, requires_grad=True)
+    numerator_weights = nn.Parameter(torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5], dtype=torch.float32, device='cuda'), requires_grad=True)
     denominator_weights = nn.Parameter(torch.tensor([1.0, 2.0, 3.0, 4.0], dtype=torch.float32, device='cuda'), requires_grad=True)
 
     # Input tensor
