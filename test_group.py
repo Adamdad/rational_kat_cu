5c54668d44332e4a657e303aa9c924058b0d1ddc
Adamdad <787707188@qq.com>
2024-07-29 11:23:04 +0800

    x = torch.rand(512, 77, 640, dtype=torch.float32, device='cuda')

--- test_group.py
+++ test_group.py
@@ -216,7 +216,7 @@ if __name__=="__main__":
     # denominator_weights = nn.Parameter(torch.tensor([1.0, 2.0, 3.0, 4.0], dtype=torch.float32, device='cuda'), requires_grad=True)
 
     # Input tensor
-    x = torch.rand(128, 77, 640, dtype=torch.float32, device='cuda')
+    x = torch.rand(512, 77, 640, dtype=torch.float32, device='cuda')
     benchmark_forward(x, numerator_weights, denominator_weights, group_size)
     
     
