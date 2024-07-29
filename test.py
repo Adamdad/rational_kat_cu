895cdc720aab0ebe208bafe37e8c0cc95dab5b32
Adamdad <787707188@qq.com>
2024-07-29 17:35:03 +0800

    test_backward(x, numerator_weights, denominator_weights)

--- test.py
+++ test.py
@@ -264,7 +264,7 @@ if __name__=="__main__":
     x = torch.randn(1024, 10, 640, dtype=torch.float32, device='cuda')
     
     
-    test_forward(x, numerator_weights, denominator_weights)
+    # test_forward(x, numerator_weights, denominator_weights)
 
     test_backward(x, numerator_weights, denominator_weights)
     # rat = Rational(cuda=True)
