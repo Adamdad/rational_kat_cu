e885356cebe8f3ccc45fada51304307824fd358a
Adamdad <787707188@qq.com>
2024-07-29 16:07:49 +0800

    numerator_weights = nn.Parameter(torch.randn(4, 6, dtype=torch.float32, device='cuda')/640, requires_grad=True)

--- test_group.py
+++ test_group.py
@@ -273,8 +273,8 @@ if __name__=="__main__":
     group_size = 4
     # Define tensors for the numerator and denominator coefficients
     # numerator of size (group_size, 5) and denominator of size (group_size, 4)
-    numerator_weights = nn.Parameter(torch.randn(4, 6, dtype=torch.float32, device='cuda'), requires_grad=True)
-    denominator_weights = nn.Parameter(torch.randn(4, 4, dtype=torch.float32, device='cuda'), requires_grad=True)
+    numerator_weights = nn.Parameter(torch.randn(4, 6, dtype=torch.float32, device='cuda')/640, requires_grad=True)
+    denominator_weights = nn.Parameter(torch.randn(4, 4, dtype=torch.float32, device='cuda')/640, requires_grad=True)
     # numerator_weights = nn.Parameter(torch.tensor([0.1, 0.2, 0.3, 0.4, 0.5], dtype=torch.float32, device='cuda'), requires_grad=True)
     # denominator_weights = nn.Parameter(torch.tensor([1.0, 2.0, 3.0, 4.0], dtype=torch.float32, device='cuda'), requires_grad=True)
 
