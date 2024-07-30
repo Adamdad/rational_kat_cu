2a6df6105bdbe022891f3c8cdbe05b38f0628bd7
Adamdad <787707188@qq.com>
2024-07-30 17:21:25 +0800

    test_backward(x, numerator_weights, denominator_weights, group_size)

--- tests/test_group.py
+++ tests/test_group.py
@@ -333,7 +333,7 @@ def benchmark_forward(x, numerator_weights, denominator_weights, group_size=4):
     return result
 
 if __name__=="__main__":
-    group_size = 4
+    group_size = 8
     # Define tensors for the numerator and denominator coefficients
     # numerator of size (group_size, 5) and denominator of size (group_size, 4)
     numerator_weights = nn.Parameter(torch.tensor([
@@ -344,58 +344,22 @@ if __name__=="__main__":
                 0.13022718688035761,
                 0.024355900098993424,
                 0.00290283948155535
-            ],[
-                -0.0012423594497499122,
-                0.5080497063245629,
-                0.41586363182937475,
-                0.13022718688035761,
-                0.024355900098993424,
-                0.00290283948155535
-            ],[
-                -0.0012423594497499122,
-                0.5080497063245629,
-                0.41586363182937475,
-                0.13022718688035761,
-                0.024355900098993424,
-                0.00290283948155535
-            ],[
-                -0.0012423594497499122,
-                0.5080497063245629,
-                0.41586363182937475,
-                0.13022718688035761,
-                0.024355900098993424,
-                0.00290283948155535
-            ]], dtype=torch.float32, device='cuda'), requires_grad=True)
+            ]]*group_size, dtype=torch.float32, device='cuda'), requires_grad=True)
     denominator_weights = nn.Parameter(torch.tensor([[
                 -0.06675015696494944,
                 0.17927646217001553,
                 0.03746682605496631,
                 1.6561610853276082e-10
-            ],[
-                -0.06675015696494944,
-                0.17927646217001553,
-                0.03746682605496631,
-                1.6561610853276082e-10
-            ],[
-                -0.06675015696494944,
-                0.17927646217001553,
-                0.03746682605496631,
-                1.6561610853276082e-10
-            ],[
-                -0.06675015696494944,
-                0.17927646217001553,
-                0.03746682605496631,
-                1.6561610853276082e-10
-            ]], dtype=torch.float32, device='cuda'), requires_grad=True)
+            ]] * group_size, dtype=torch.float32, device='cuda'), requires_grad=True)
     # numerator_weights.data[1] *= 2
     # numerator_weights.data[2] *= 3
     # numerator_weights.data[3] *= 4
 
     # Input tensor
     x = torch.randn(64, 256, 320, dtype=torch.float32, device='cuda')
-    test_forward(x, numerator_weights, denominator_weights, group_size)
+    # test_forward(x, numerator_weights, denominator_weights, group_size)
     # benchmark_forward(x, numerator_weights, denominator_weights, group_size)
-    # test_backward(x, numerator_weights, denominator_weights, group_size)
+    test_backward(x, numerator_weights, denominator_weights, group_size)
     # benchmark_backward(x, numerator_weights, denominator_weights, group_size)
     
     
