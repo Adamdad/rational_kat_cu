a0878c7eee54b3a4ed7c8739e4c8b64fa1d7e95a
Adamdad <787707188@qq.com>
2024-08-26 11:20:14 +0800

    torch::Tensor rational_fwd_1dgroup(

--- src/utils.h
+++ src/utils.h
@@ -5,25 +5,25 @@
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 #define CHECK_LESS(x, y) TORCH_CHECK(x < y, #x " should be less than " #y)
 
-torch::Tensor rational_fwd(
-  torch::Tensor x, 
-  torch::Tensor n, 
-  torch::Tensor d);
+// torch::Tensor rational_fwd(
+//   torch::Tensor x, 
+//   torch::Tensor n, 
+//   torch::Tensor d);
 
-torch::Tensor rational_fwd_cuda(
-  torch::Tensor x, 
-  torch::Tensor n, 
-  torch::Tensor d);
+// torch::Tensor rational_fwd_cuda(
+//   torch::Tensor x, 
+//   torch::Tensor n, 
+//   torch::Tensor d);
 
-torch::Tensor rational_fwd_optimized(
-  torch::Tensor x, 
-  torch::Tensor n, 
-  torch::Tensor d);
+// torch::Tensor rational_fwd_optimized(
+//   torch::Tensor x, 
+//   torch::Tensor n, 
+//   torch::Tensor d);
 
-torch::Tensor rational_fwd_cuda_optimized(
-  torch::Tensor x, 
-  torch::Tensor n, 
-  torch::Tensor d);
+// torch::Tensor rational_fwd_cuda_optimized(
+//   torch::Tensor x, 
+//   torch::Tensor n, 
+//   torch::Tensor d);
 
 torch::Tensor rational_fwd_1dgroup(
   torch::Tensor x, 
@@ -37,29 +37,29 @@ torch::Tensor rational_fwd_cuda_1dgroup(
   torch::Tensor d, 
   int group);
 
-std::vector<torch::Tensor> rational_bwd(
-  torch::Tensor dy, 
-  torch::Tensor x, 
-  torch::Tensor n, 
-  torch::Tensor d);
+// std::vector<torch::Tensor> rational_bwd(
+//   torch::Tensor dy, 
+//   torch::Tensor x, 
+//   torch::Tensor n, 
+//   torch::Tensor d);
 
-std::vector<torch::Tensor> rational_bwd_cuda(
-    torch::Tensor dy, 
-    torch::Tensor x, 
-    torch::Tensor n, 
-    torch::Tensor d);
+// std::vector<torch::Tensor> rational_bwd_cuda(
+//     torch::Tensor dy, 
+//     torch::Tensor x, 
+//     torch::Tensor n, 
+//     torch::Tensor d);
 
-std::vector<torch::Tensor> rational_bwd_optimized(
-    torch::Tensor dy, 
-    torch::Tensor x, 
-    torch::Tensor n, 
-    torch::Tensor d);
+// std::vector<torch::Tensor> rational_bwd_optimized(
+//     torch::Tensor dy, 
+//     torch::Tensor x, 
+//     torch::Tensor n, 
+//     torch::Tensor d);
 
-std::vector<torch::Tensor> rational_bwd_cuda_optimized(
-    torch::Tensor dy, 
-    torch::Tensor x, 
-    torch::Tensor n, 
-    torch::Tensor d);
+// std::vector<torch::Tensor> rational_bwd_cuda_optimized(
+//     torch::Tensor dy, 
+//     torch::Tensor x, 
+//     torch::Tensor n, 
+//     torch::Tensor d);
 
 std::vector<torch::Tensor> rational_bwd_1dgroup(
   torch::Tensor dy, 
