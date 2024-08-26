b4f782ac26ef1e366fad152e0107630b3ab9b268
Adamdad <787707188@qq.com>
2024-08-26 11:13:50 +0800

    KAT_Group

--- kat_rational/kat_1dgroup.py
+++ kat_rational/kat_1dgroup.py
@@ -103,6 +103,74 @@ class KAT_1DGroup(nn.Module):
             str: String representation of the module's configuration.
         """
         return f'num_groups={self.num_groups}, order={self.order}'
+
+class KAT_Group(nn.Module):
+    def __init__(self, num_groups=4, mode="gelu"):
+        """
+        Initialize the KAT_1DGroup module.
+
+        Args:
+            num_groups (int): Number of groups to divide the input for separate processing.
+            init_mode (str): Initialization mode which determines the preset weights from JSON file.
+        """
+        super(KAT_Group, self).__init__()
+        self.order = (5, 4)
+        self.num_groups = num_groups
+        # Initialize parameters for each group
+        self.initialize(mode=mode)
+        
+    def initialize(self, mode="gelu"):
+        """
+        Initialize weights from a JSON file based on the specified mode.
+
+        Args:
+            mode (str): The initialization mode.
+        """
+        cfd = os.path.dirname(os.path.realpath(__file__))
+        try:
+            with open(f'{cfd}/init.json') as json_file:
+                data = json.load(json_file)
+            weight_numerator = torch.tensor(data[mode]["init_w_numerator"])
+            weight_numerator = torch.cat([weight_numerator]).view(1, -1)
+            weight_denominator = torch.tensor(data[mode]["init_w_denominator"])
+            weight_denominator = torch.cat([weight_denominator]*self.num_groups).view(self.num_groups, -1)
+             
+            self.weight_numerator = nn.Parameter(torch.FloatTensor(weight_numerator)
+                                                      , requires_grad=True) 
+            self.weight_denominator = nn.Parameter(torch.FloatTensor(weight_denominator)
+                                                      , requires_grad=True) 
+
+        except FileNotFoundError:
+            print("Initialization JSON file not found.")
+        except json.JSONDecodeError:
+            print("Error decoding JSON.")
+    
+    def forward(self, input):
+        """
+        Forward pass of the module.
+
+        Args:
+            input (Tensor): Input tensor.
+
+        Returns:
+            Tensor: Processed tensor.
+        """
+
+        assert input.dim() == 3, "Input tensor must be 3D. Of size (batch, length, channels)."
+
+        # select the first group, and repeat the weights for all groups
+        weight_numerator = self.weight_numerator.repeat(self.num_groups, 1)
+        return rational_1dgroup.apply(input, weight_numerator, self.weight_denominator, self.num_groups)
+        
+    
+    def extra_repr(self):
+        """
+        Extra representation of the module for debugging.
+
+        Returns:
+            str: String representation of the module's configuration.
+        """
+        return f'num_groups={self.num_groups}, order={self.order}'
     
 class KAT_1DGroupv2(nn.Module):
     def __init__(self, num_groups=4, mode="gelu"):
